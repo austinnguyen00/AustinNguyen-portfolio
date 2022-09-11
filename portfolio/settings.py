c1be47d835a922790f3a6c2549e58a57cb209519
Austin <ndkhiem3010@gmail.com>
2022-09-11 13:43:51 +0800

    Update STATIC in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,14 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = "/staticfiles/"
 MEDIA_URL = "images/"
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "staticfiles")
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles")
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
